cfcf2c1b1af8d14929ca03a9bd69b3cf3b52eaf9
Tribruin <rob@blount-family.com>
2020-12-12 06:54:31 -0600

    Day 12 Complete

--- Day11/main-alt.py
+++ Day11/main-alt.py
@@ -12,29 +12,35 @@ def print_array(array):
     print("")
 
 
-def Part1(input):
-    # Fill the seats
-    seating = [list(x) for x in input]
+def Part1():
+    def check_surrounding_seats(r, c) -> int:
+        count = -1 if current_seating[r][c] == "#" else 0
+        count += sum(
+            [col[c - 1 : c + 2].count("#") for col in current_seating[r - 1 : r + 2]]
+        )
+        # count += sum([x.count("#") for x in surround])
+
+        if current_seating[r][c] == "L" and count == 0:
+            seating[r][c] = "#"
+        elif current_seating[r][c] == "#" and count >= 4:
+            seating[r][c] = "L"
+        return
+
+    # create the seating arrangement and add an floor around the edges
+    seating = [list(x) for x in input_data]
+    seating = [["."] + x + ["."] for x in seating]
+    seating = [["."] * len(seating[0])] + seating + [["."] * len(seating[0])]
+    # print_array(seating)
+
     rows = len(seating)
     columns = len(seating[0])
 
     while True:
         current_seating = deepcopy(seating)
-        for r in range(rows):
-            for c in range(columns):
+        for r in range(1, rows - 1):
+            for c in range(1, columns - 1):
                 if not current_seating[r][c] == ".":
-                    count = 0
-                    for r1 in range(-1, 2):
-                        for c1 in range(-1, 2):
-                            if (0 <= (r + r1) < rows) and (0 <= (c + c1) < columns):
-                                if current_seating[r + r1][c + c1] == "#":
-                                    count += 1
-                    if current_seating[r][c] == "#":
-                        count -= 1
-                    if current_seating[r][c] == "L" and count == 0:
-                        seating[r][c] = "#"
-                    elif current_seating[r][c] == "#" and count >= 4:
-                        seating[r][c] = "L"
+                    check_surrounding_seats(r, c)
         # print_array(seating)
         # print(current_seating == seating)
         if current_seating == seating:
@@ -42,9 +48,9 @@ def Part1(input):
     print(sum([x.count("#") for x in current_seating]))
 
 
-def Part2(input):
+def Part2():
 
-    seating = [list(x) for x in input]
+    seating = [list(x) for x in input_data]
     rows = len(seating)
     columns = len(seating[0])
     # print_array(seating)
@@ -92,5 +98,5 @@ def Part2(input):
 
 a = AOC(11, False)
 input_data = a.read_lines()
-Part1(input_data)
-# Part2(input_data)
+Part1()
+# Part2()
---
+++ Day12/main.py
@@ -0,0 +1,98 @@
+import sys
+
+sys.path.append("/Users/rblount/Scripts/AdventOfCode/2020/tools")
+
+from AOC import AOC
+
+move_options = {"N": (0, -1), "E": (1, 0), "S": (0, 1), "W": (-1, 0)}
+move_directions = list(move_options.values())
+dir_chars = ["N", "E", "S", "W"]
+turn_chars = ["L", "R"]
+move_char = ["F"]
+
+
+class Ferry_Nav:
+    def __init__(self):
+        self.x, self.y = 0, 0
+        self.wp_x, self.wp_y = 10, -1
+        self.direction = "E"
+        self.move_x, self.move_y = move_options[self.direction]
+
+    def turn(self, direction, degrees):
+        for _ in range(degrees // 90):
+            if direction == "R":
+                self.move_x, self.move_y = -self.move_y, self.move_x
+            else:
+                self.move_x, self.move_y = self.move_y, -self.move_x
+
+        self.direction = dir_chars[move_directions.index((self.move_x, self.move_y))]
+        return
+
+    def move_direction(self, direction, move_value):
+        x1, y1 = move_options[direction]
+        self.x, self.y = self.x + x1 * move_value, self.y + y1 * move_value
+
+    def forward(self, move_value):
+        x1, y1 = move_options[self.direction]
+        self.x, self.y = self.x + x1 * move_value, self.y + y1 * move_value
+
+    def manhattan_dist(self):
+        return abs(self.x) + abs(self.y)
+
+    def move_waypoint(self, direction, move_value):
+        x1, y1 = move_options[direction]
+        self.wp_x, self.wp_y = self.wp_x + x1 * move_value, self.wp_y + y1 * move_value
+
+    def forward_part2(self, move_value):
+        # x1, y1 = move_options[self.direction]
+        self.x, self.y = (
+            self.x + self.wp_x * move_value,
+            self.y + self.wp_y * move_value,
+        )
+
+    def turn_waypoint(self, direction, degrees):
+        for _ in range(degrees // 90):
+            if direction == "R":
+                self.wp_x, self.wp_y = -self.wp_y, self.wp_x
+            else:
+                self.wp_x, self.wp_y = self.wp_y, -self.wp_x
+
+
+def part1():
+
+    ferry = Ferry_Nav()
+    for step in steps:
+        char, value = step["move"], step["value"]
+        print(f"{char}{value}: ", end="")
+
+        if char in dir_chars:
+            ferry.move_direction(char, value)
+        elif char in turn_chars:
+            ferry.turn(char, value)
+        else:
+            ferry.forward(value)
+        print(f"x={ferry.x} y={ferry.y} dir={ferry.direction}")
+
+    print(ferry.x, ferry.y, ferry.manhattan_dist())
+
+
+def part2():
+    ferry = Ferry_Nav()
+    for step in steps:
+        char, value = step["move"], step["value"]
+        print(f"{char}{value}: ", end="")
+        if char in dir_chars:
+            ferry.move_waypoint(char, value)
+        elif char in turn_chars:
+            ferry.turn_waypoint(char, value)
+        else:
+            ferry.forward_part2(value)
+        print(f"x={ferry.x} y={ferry.y} wp_x = {ferry.wp_x} wp_y = {ferry.wp_y}")
+
+    print(ferry.x, ferry.y, ferry.manhattan_dist())
+
+
+a = AOC(12, test=False)
+steps = [{"move": x[0], "value": int(x[1:])} for x in a.read_lines()]
+part1()
+part2()
